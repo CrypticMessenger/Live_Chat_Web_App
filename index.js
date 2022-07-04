48998f73c640d9fa65e9829f11c6a574aa0de835
CrypticMessenger <2020csb1072@iitrpr.ac.in>
2022-07-05 02:12:32 +0530

    changed

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV == 'production'){
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('/public/build'));
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'public/build', 'index.html'));
-  });
+  // app.get('*', (request, response) => {
+  //   response.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+  // });
 }
 
 
